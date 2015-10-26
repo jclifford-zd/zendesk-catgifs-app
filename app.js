1b9071806f50f0c0fec6211fe07a75c0704b8c23
John Clifford <onesoma@gmail.com>
2015-10-26 12:04:47 +0000

    Adds request and callbacks, log success or error message

--- app.js
+++ app.js
@@ -2,11 +2,35 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'getGifs',
+      'fetchGifs.done': 'processGifs',
+      'fetchGifs.fail': 'processError',
     },
 
-    doSomething: function() {
-    }
+    getGifs: function() {
+        this.ajax('fetchGifs');
+    },
+
+    requests: {
+        fetchGifs: {
+            url: 'https://api.imgur.com/3/gallery/r/CatGifs',
+            type: 'GET',
+            dataType: 'json',
+            headers: {
+                'Authorization': 'Client-ID 5113aab57a5e6c8'
+            }
+        }
+    },
+
+    processGifs: function(data) {
+        // process and display a gif
+        console.log('request succeeded');
+    },
+
+    processError: function() {
+        // process request failure
+        console.log('request failed');
+    },
   };
 
 }());
